eef44260605c5a4ca0597c920cd65ec8bd845e1f
Michele Lindroos <michele.lindroos@omoroi.fi>
2022-10-25 17:37:49 +0300

    scheme: add implementation

--- scheme/algorithm.scm
+++ scheme/algorithm.scm
@@ -4,6 +4,7 @@
 (use-modules
     (ice-9 exceptions)
     (ice-9 format)
+    (srfi srfi-1)
     (srfi srfi-9))
 
 (define-record-type job
@@ -65,10 +66,90 @@
 (define (make-warning-job-not-needed job-name)
     `(("message" . ,(format #f "Job '~a' is not required" job-name)) ("type" . "WARNING") ("error" . "JOB NOT REQUIRED")))
 
-(define (algorithm jobs targets)
-    (define warnings '())
+(define (algorithm jobs-input targets)
+    ; set all after-before relations
+    (define (set-after-before-pairs jobs-input job)
+        (define (get-job-name job-input)
+            (car job-input))
+        (define (job-after job-name job-input)
+            (member job-name (cdr (assoc "after" (cdr job-input)))))
+        (define (job-before job-name job-input)
+            (member job-name (cdr (assoc "before" (cdr job-input)))))
+        (let* ((job-name (car job))
+               (after-list-original (cdr (assoc "after" (cdr job))))
+               (after-list-add (map get-job-name (filter (lambda (job-input) (job-before job-name job-input)) jobs-input)))
+               (after-list (append after-list-original after-list-add))
+               (before-list-original (cdr (assoc "before" (cdr job))))
+               (before-list-add (map get-job-name (filter (lambda (job-input) (job-after job-name job-input)) jobs-input)))
+               (before-list (append before-list-original before-list-add)))
+            `(,job-name . (("after" . ,after-list) ("before" . ,before-list)))))
+    (define jobs (map (lambda (job) (set-after-before-pairs jobs-input job)) jobs-input))
 
-    ; TODO: implementation
+    ; check if a job depends on itself
+    (define (job-depends-on-itself job)
+        (let ((job-name (car job))
+              (job-value (cdr job)))
+            (if (or
+                    (member job-name (cdr (assoc "after" job-value)))
+                    (member job-name (cdr (assoc "before" job-value))))
+                (raise-exception (make-job-depends-on-itself-exception job-name)))))
+    (for-each job-depends-on-itself jobs)
+
+    ; check all referenced jobs exist
+    (define (all-referenced-jobs-exist jobs job-to-check)
+        (define (check-list job-depender the-list)
+            (for-each
+                (lambda (job-name)
+                    (if (not (assoc job-name jobs))
+                        (raise-exception (make-job-not-found-exception job-depender job-name))))
+                the-list))
+        (let ((job-name (car job-to-check))
+              (job-value (cdr job-to-check)))
+            (check-list job-name (cdr (assoc "after" job-value)))
+            (check-list job-name (cdr (assoc "before" job-value)))))
+    (for-each (lambda (job-to-check) (all-referenced-jobs-exist jobs job-to-check)) jobs)
+
+    ; check all targets exist
+    (for-each
+        (lambda (target-name)
+            (if (not (assoc target-name jobs))
+                (raise-exception (make-target-not-found-exception target-name))))
+        targets)
+
+    ; build list of jobs needed and check cyclic dependencies
+    (define (enumerate-dependencies jobs job accumulator)
+        (define (add-unique-jobs this-accumulator previous-accumulator)
+            (lset-union string= previous-accumulator this-accumulator))
+        (let* ((job-name job)
+              (job-value (cdr (assoc job jobs)))
+              (new-dependencies (cdr (assoc "after" job-value))))
+            (cond
+                ((null? new-dependencies) (list job-name))
+                ((not (null? (lset-intersection string= accumulator new-dependencies)))
+                    (raise-exception (make-cyclic-dependency-exception)))
+                (else
+                    (let ((updated-accumulator (append accumulator new-dependencies)))
+                        (fold
+                            (lambda (this previous)
+                                (lset-union string= previous (enumerate-dependencies jobs this updated-accumulator)))
+                            '()
+                            new-dependencies))))))
+    (define jobs-needed (append targets
+        (fold
+            (lambda (this previous)
+                (lset-union string= previous (enumerate-dependencies jobs this '())))
+            '()
+            targets)))
+
+    ; check all jobs are needed (issue warning if not)
+    (define (check-job-is-needed jobs-needed job)
+        (let ((job-name (car job)))
+            (member job-name jobs-needed)))
+    (define warnings
+        (filter-map
+            (lambda (job)
+                (if (not (check-job-is-needed jobs-needed job)) (make-warning-job-not-needed (car job)) #f))
+            jobs))
 
     (if (null? targets)
         (raise-exception (make-no-targets-exception)))
